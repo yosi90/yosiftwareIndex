6ad722d97a6d778352db3bd33908b4e880bdf8f1
yosi90 <yosi121990@hotmail.es>
2023-04-26 19:06:46 +0100

    Cambios en routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'rol', redirectTo: 'https://rol.yosiftware.es/' }
+    { path: 'rol', redirectTo: 'https://rol.yosiftware.es/' },
+    { path: 'cv', redirectTo: 'https://cv.yosiftware.es/' }
 ];
 
 @NgModule({
