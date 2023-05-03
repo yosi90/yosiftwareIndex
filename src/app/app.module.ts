4dde7f57b50257a0b86b66ada7422294d8c0d67a
yosi90 <yosi121990@hotmail.es>
2023-05-03 20:23:21 +0100

    Tecnologias añadidas
    Añadido python y sql server
    Agregados tooltips a las tecnologias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
     declarations: [
@@ -14,7 +15,8 @@ import { MatCardModule } from '@angular/material/card';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatCardModule
+        MatCardModule,
+        MatTooltipModule
     ],
     providers: [],
     bootstrap: [AppComponent]
