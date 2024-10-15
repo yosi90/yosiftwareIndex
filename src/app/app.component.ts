7e52729f758adc56d8153655580f3a97432af8e1
yosi90 <yosi121990@hotmail.es>
2024-10-15 17:34:25 +0100

    Mejoras de diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
     title = 'Yosiftware';
+    resetVisible: boolean = false;
 
     ngOnInit(): void {
         const cards: NodeListOf<HTMLElement> = document.querySelectorAll(".card");
@@ -19,18 +20,27 @@ export class AppComponent implements OnInit {
                     const cardCenterY: number = cardPosition.top + cardPosition.height / 2;
                     const cardRotateX: number = (e.pageY - cardCenterY) / 30;
                     const cardRotateY: number = (e.pageX - cardCenterX) / -30;
-                    const shadowX: number = cardRotateX * -6;
-                    const shadowY: number = cardRotateY * -6;
+                    const shadowX: number = cardRotateX * -11;
+                    const shadowY: number = cardRotateY * -11;
 
-                    card.style.boxShadow = `0px 5px 10px rgb(14, 13, 13), 5px 0px 10px rgb(14, 13, 13), 0px -5px 10px rgb(46, 24, 24), -5px 0px 10px rgb(46, 24, 24), ${shadowY}px ${shadowX}px 20px rgba(0, 0, 0, .3)`;
-                    card.style.transform = `perspective(500px) rotateX(${cardRotateX}deg) rotateY(${cardRotateY}deg) translateZ(50px)`;
+                    card.style.boxShadow = `0px 10px 15px rgb(14, 13, 13), 10px 0px 15px rgb(14, 13, 13), 0px -10px 15px rgb(46, 24, 24), -10px 0px 15px rgb(46, 24, 24), ${shadowY}px ${shadowX}px 20px rgba(0, 0, 0, .3)`;
+                    card.style.transform = `perspective(500px) rotateX(${cardRotateX}deg) rotateY(${cardRotateY}deg)`;
                 });
 
                 card.addEventListener("mouseleave", () => {
-                    card.style.transform = "none";
-                    card.style.boxShadow = "5px 5px 10px rgb(46, 35, 35), -5px -5px 10px rgb(46, 35, 35), 5px -5px 10px rgb(46, 35, 35), -5px 5px 10px rgb(46, 35, 35)";
+                    // card.style.transform = "none";
+                    this.resetVisible = true;
+                    card.style.boxShadow = "2px 2px 5px rgba(0, 0, 0, 0.4), -2px -2px 5px rgba(0, 0, 0, 0.4)";
                 });
             });
         }
+    }
+
+    resetCards() {
+        const cards: NodeListOf<HTMLElement> = document.querySelectorAll(".card");
+        cards.forEach(c => {
+            c.style.transform = "none";
+        });
+        this.resetVisible = false;
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
         AppRoutingModule,
         BrowserAnimationsModule,
         MatCardModule,
-        MatTooltipModule
+        MatTooltipModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
